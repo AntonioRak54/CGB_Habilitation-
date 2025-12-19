4e6b97f8dd0626549acde5a021dcb53d66b82470
AntonioRak54 <antonioniriantsoarakotomalala@gmail.com>
2025-12-19 09:16:27 +0300

    Bug dans le Views de Admin

--- obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
+++ obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001ebad823ef1edc8caf4624e1915eeadbd6ac0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bf8f60e5ee90b0e63bc6519dcb2a753e9c9387")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

3255d78e6b68aeedc2195a739b75b5d8ed4ae02d
AntonioRak54 <antonioniriantsoarakotomalala@gmail.com>
2026-01-05 16:03:35 +0300

    Les liens dans les logins avec commentaires

--- obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
+++ obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963fcd5d079785a7d87225eaa05436e7fa12c4fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e45c610d118034cb48210b42ab422dc664b5d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

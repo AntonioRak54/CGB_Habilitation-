e81fa7a0bfbadd6bb7ba345668a2ff88b6a10ff2
AntonioRak54 <antonioniriantsoarakotomalala@gmail.com>
2026-01-06 11:31:48 +0300

    Reinitialisation des migrations

--- obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
+++ obj/Debug/net9.0/CGB_Habilitation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3255d78e6b68aeedc2195a739b75b5d8ed4ae02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee36984277dfbc16f92857f601656586fe46e6d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGB_Habilitation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
